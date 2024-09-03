ba80c9c0967efdc69bbe26976ea5368c8fc0e30c
PhuongLe170 <91200366+PhuongLe170@users.noreply.github.com>
2024-09-03 20:37:38 +0700

    add new code change. (#29)
    Co-authored-by: phuongthanhle <phuongthanhle@craniumai.onmicrosoft.com>

--- src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
+++ src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
-      "Windy"
+      "Cool"
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
